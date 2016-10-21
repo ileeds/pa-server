0ae01d8767d0267d6d2fd993ef2cb999853a0e69
Ian Leeds <ileeds@brandeis.edu>
2016-10-21 17:55:35 -0400

    neat

--- src/edu/Brandeis/cs131/Common/IanLeeds/BasicServer.java
+++ src/edu/Brandeis/cs131/Common/IanLeeds/BasicServer.java
@@ -14,13 +14,14 @@ public class BasicServer extends Server {
 	}
 
 	@Override
-	public boolean connectInner(Client client) {
+	//synchronized so only one thread can perform at a time
+	public synchronized boolean connectInner(Client client) {
 		//if no accessing clients
 		if (accessing.isEmpty()){
 			accessing.add(client);
 			return true;
-		//if only one client already accessing, this client is a shared client, and they don't have shared industry
-		}else if (accessing.size()==1 && client instanceof SharedClient && !accessing.get(0).getIndustry().equals(client.getIndustry())){
+		//if only one client already accessing, it is a shared client, this client is also a shared client, and they don't have shared industry
+		}else if (accessing.size()==1 && accessing.get(0) instanceof SharedClient && client instanceof SharedClient && !accessing.get(0).getIndustry().equals(client.getIndustry())){
 			accessing.add(client);
 			return true;
 		}
@@ -29,7 +30,8 @@ public class BasicServer extends Server {
 	}
 
 	@Override
-	public void disconnectInner(Client client) {
+	//synchronized so only one thread can perform at a time
+	public synchronized void disconnectInner(Client client) {
 		//removes client from list of accessing clients
 		accessing.remove(client);
 	}
